0257c47851b23f9b85560f38680503e7f2fbad8c
Borislav Pecherskikh <borislavtarget@gmail.com>
2022-12-04 23:27:36 +0500

    Repair first errors

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # Из модуля models импортируем модель Post
-from .models import Group, Post 
+from .models import Group, Post
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -11,16 +11,18 @@ class PostAdmin(admin.ModelAdmin):
         'pub_date',
         'author',
         'group',
-        ) 
+    )
     list_editable = ('group',)
     # Добавляем интерфейс для поиска по тексту постов
-    search_fields = ('text',) 
+    search_fields = ('text',)
     # Добавляем возможность фильтрации по дате
-    list_filter = ('pub_date',) 
-    # Это свойство сработает для всех колонок: где пусто — там будет эта строка 
-    empty_value_display = '-пусто-'  
+    list_filter = ('pub_date',)
+    # Это свойство сработает для всех колонок: где пусто — там будет эта строка
+    empty_value_display = '-пусто-'
 
 # При регистрации модели Post источником конфигурации для неё назначаем
 # класс PostAdmin
+
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
 from .models import Group, Post
+
 
 def index(request):
     template = 'posts/index.html'
     title = 'Yatube для авторов'
     # Одна строка вместо тысячи слов на SQL:
     # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    # отсортированных по полю pub_date по убыванию
+    # (от больших значений к меньшим)
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'posts': posts,
         'text': title,
     }
-    return render(request, template, context) 
+    return render(request, template, context)
+
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     title = 'Информация о группах проекта Yatube'
-    # Функция get_object_or_404 получает по заданным критериям объект 
+    # Функция get_object_or_404 получает по заданным критериям объект
     # из базы данных или возвращает сообщение об ошибке, если объект не найден.
     # В нашем случае в переменную group будут переданы объекты модели Group,
     # поле slug у которых соответствует значению slug в запросе
@@ -33,5 +35,4 @@ def group_posts(request, slug):
         'posts': posts,
         'text': title,
     }
-    return render(request, template, context) 
-    return render(request, template, context) 
+    return render(request, template, context)
