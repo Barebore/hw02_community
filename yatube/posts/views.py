8039ab719035b7eb8741d44a615dbcf99e410d0d
Borislav Pecherskikh <borislavtarget@gmail.com>
2022-12-08 19:40:55 +0500

    Ver2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,16 +3,13 @@ from .models import Group, Post
 
 
 def index(request):
+    POSTS_ON_PAGE = 10
     template = 'posts/index.html'
-    title = 'Yatube для авторов'
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию
-    # (от больших значений к меньшим)
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.select_related()[:POSTS_ON_PAGE]
+    group = 'Не получается сюда затащить номер группы'
     context = {
         'posts': posts,
-        'text': title,
+        'group': group,
     }
     return render(request, template, context)
 
@@ -20,16 +17,8 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     title = 'Информация о группах проекта Yatube'
-    # Функция get_object_or_404 получает по заданным критериям объект
-    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
-    # В нашем случае в переменную group будут переданы объекты модели Group,
-    # поле slug у которых соответствует значению slug в запросе
     group = get_object_or_404(Group, slug=slug)
-
-    # Метод .filter позволяет ограничить поиск по критериям.
-    # Это аналог добавления
-    # условия WHERE group_id = {group_id}
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group)[:10]
     context = {
         'group': group,
         'posts': posts,
