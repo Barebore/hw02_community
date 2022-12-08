c1604b66f25a56ff29a43d0f1e1e253a2c363c8f
Borislav Pecherskikh <borislavtarget@gmail.com>
2022-12-09 02:49:25 +0500

    Ver3.0

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,27 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Group, Post
+POSTS_ON_PAGE = 10
 
 
 def index(request):
-    POSTS_ON_PAGE = 10
     template = 'posts/index.html'
     posts = Post.objects.select_related()[:POSTS_ON_PAGE]
-    group = 'Не получается сюда затащить номер группы'
     context = {
         'posts': posts,
-        'group': group,
     }
     return render(request, template, context)
 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    title = 'Информация о группах проекта Yatube'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group)[:10]
+    posts = group.posts.all()[:POSTS_ON_PAGE]
     context = {
         'group': group,
         'posts': posts,
-        'text': title,
     }
     return render(request, template, context)
